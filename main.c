17efd7a77212d9a6713f4a230cce5f5e15530492
Kefka <50819728+Kefka-dev@users.noreply.github.com>
2023-02-25 02:00:46 +0100

    nacitanie vstupov, testovaci vypis

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+
+    //M - vyska hypo, Nr - splatnost hyp. v rokoch, ir - urok. sazd
+    double M, Nr, ir;
+
+    scanf("%lf %lf %lf", &M, &Nr, &ir);
+    printf("%.2lf\n", M);
+    printf("%.2lf\n", Nr);
+    printf("%.2lf\n", ir);
+
+    
+
+    return 0;
+}
